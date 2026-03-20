4aa1dcd78553c4fcc67c0638b8cd691212272c11
Hamsan2013 <hamsandivya@gmail.com>
2026-03-20 14:10:36 +0530

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,32 +1,19 @@
-; Wendigo OS Bootloader
-
 [org 0x7c00]
 
-mov ah, 0x0e
-mov si, message
+mov ah,0x0e
+mov si,msg
 
 print:
 lodsb
-cmp al, 0
-je load_kernel
+cmp al,0
+je halt
 int 0x10
 jmp print
 
-load_kernel:
+halt:
+jmp $
 
-mov bx, 0x1000
-mov dh, 1
-
-mov ah, 0x02
-mov al, dh
-mov ch, 0
-mov cl, 2
-mov dh, 0
-int 0x13
-
-jmp 0x0000:0x1000
-
-message db "Booting Wendigo OS...",0
+msg db "Booting Wendigo OS...",0
 
 times 510-($-$$) db 0
 dw 0xaa55
