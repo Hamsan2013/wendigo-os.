6fa69e193b1de27a21845f0372a240b092792c0c
Hamsan2013 <hamsandivya@gmail.com>
2026-03-20 14:51:24 +0530

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,19 +1,30 @@
 [org 0x7c00]
 
-mov ah,0x0e
-mov si,msg
+; print boot message
+mov ah, 0x0e
+mov si, bootmsg
 
 print:
 lodsb
-cmp al,0
-je halt
+cmp al, 0
+je load_kernel
 int 0x10
 jmp print
 
-halt:
-jmp $
+load_kernel:
+; load 1 sector (kernel) from disk into memory at 0x1000
+mov bx, 0x1000
+mov ah, 0x02
+mov al, 1        ; number of sectors
+mov ch, 0
+mov cl, 2        ; sector 2 (kernel)
+mov dh, 0
+mov dl, 0x00
+int 0x13
 
-msg db "Booting Wendigo OS...",0
+jmp 0x0000:0x1000
+
+bootmsg db "Booting Wendigo OS...",0
 
 times 510-($-$$) db 0
 dw 0xaa55
