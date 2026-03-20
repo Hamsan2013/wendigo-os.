755f308ae4b420343a543913e6a242189f86d0d1
Hamsan2013 <hamsandivya@gmail.com>
2026-03-20 15:18:38 +0530

    Update kernel.asm

--- kernel.asm
+++ kernel.asm
@@ -1,9 +1,34 @@
 [org 0x1000]
 
-mov ah,0x0e
+; switch to VGA 320x200 graphics
+mov ax, 0x0013
+int 0x10
+
+; video memory location
+mov ax, 0xA000
+mov es, ax
+xor di, di
+
+mov si, logo_data
+mov cx, 64000
+
+draw:
+lodsb
+stosb
+loop draw
+
+; wait for key
+mov ah,0
+int 0x16
+
+; return to text mode
+mov ax,0x0003
+int 0x10
+
 mov si,msg
 
 print:
+mov ah,0x0e
 lodsb
 cmp al,0
 je halt
@@ -13,17 +38,7 @@ jmp print
 halt:
 jmp $
 
-msg db "Wendigo OS Kernel Loaded!",0
-print_home:
-lodsb
-cmp al,0
-je halt
-int 0x10
-jmp print_home
+msg db "Wendigo OS Starting...",0
 
-halt:
-jmp $
-
-logo db "WENDIGO OS",13,10,13,10,0
-lockmsg db "Lock Screen - Press any key to open",13,10,0
-home db "Home Screen",13,10,"[ Forest Store ]",13,10,"forest-yum-store.lovable.app",0
+logo_data:
+incbin "logo.raw"
